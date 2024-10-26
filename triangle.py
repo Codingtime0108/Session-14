60b0fad2c745442b96cc9bb7225ec9e1a8d168b6
Codingtime <13tia.vora@dulwich-seoul.kr>
2024-10-26 20:18:44 +0900

    triangle

---
+++ triangle.py
@@ -0,0 +1,6 @@
+print("Right angled triangle pattern with stars (*)")
+n = int(input("Enter the number of rows: "))
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
